490facd228d3f92a004fcae4033bd51d4cdd1fd6
retroplasma <46618410+retroplasma@users.noreply.github.com>
2019-07-02 18:09:12 +0200

    Add EnumerateFileChunks helper

--- src/Binsync.Core/Helpers/General.cs
+++ src/Binsync.Core/Helpers/General.cs
@@ -1,4 +1,5 @@
 using System;
+using System.IO;
 
 namespace Binsync.Core.Helpers
 {
@@ -58,6 +59,37 @@ namespace Binsync.Core.Helpers
 		{
 			using (var ms = new System.IO.MemoryStream(b))
 				return ProtoBuf.Serializer.Deserialize<T>(ms);
+		}
+
+		public class FileChunk { public byte[] Bytes; public long Position; }
+
+		public static System.Collections.Generic.IEnumerable<FileChunk> EnumerateFileChunks(string localPath, int chunkSize)
+		{
+			using (var fs = new FileStream(localPath, FileMode.Open, FileAccess.Read, FileShare.Read))
+			{
+				bool eof = false;
+				while (!eof)
+				{
+					var fspos = fs.Position;
+					byte[] bytes;
+					using (var ms = new MemoryStream())
+					{
+						const int bufsize = 4096;
+						byte[] buffer = new byte[bufsize];
+						int read = 0;
+						int readSum = 0;
+						while (readSum < chunkSize - bufsize && (read = fs.Read(buffer, 0, bufsize)) > 0)
+						{
+							ms.Write(buffer, 0, read);
+							readSum += read;
+						}
+
+						eof |= fs.Position == fs.Length;
+						bytes = ms.ToArray();
+					}
+					yield return new FileChunk { Bytes = bytes, Position = fspos };
+				}
+			}
 		}
 	}
 }
